5d7952373c2e8a03a76335527d40beea2f5332b3
pradumang84 <pradumang84@gmail.com>
2025-07-29 22:19:33 +0530

    Add files via upload

---
+++ 24U030067 LAB 1.py
@@ -0,0 +1,13 @@
+marks=int(input("ENTER THE MARKS OF STUDENT"))
+if 90<=marks<=100:#Grade "A" 
+          print("A")
+elif 75<=marks<89:#Grade "B"
+               print("B")
+elif 60<=marks<74: #Grade "C"
+         print("C")
+elif 40<=marks<59: #Grade "D"
+         print("D") 
+elif 0<=marks<39: #Grade "E"
+         print("F")
+else:
+        print("invalid input ")
